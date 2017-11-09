8815950207b76ea9707a3cafdcb2c6ed960f26ae
DESKTOP-2DC3SS2\L-kaxy <770619071@qq.com>
2017-11-09 15:21:57 +0800

    no message

---
+++ HiveJdbcClient.java
@@ -0,0 +1,35 @@
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.DriverManager;
+ 
+public class HiveJdbcClient {
+  private static final String DIRVERNAME = "org.apache.hive.jdbc.HiveDriver";
+ 
+  /**
+   * @param args
+   * @throws SQLException
+   */
+  public static void main(String[] args) throws SQLException {
+      try {
+      Class.forName(DIRVERNAME);
+    } catch (ClassNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      System.exit(1);
+    }
+    //replace "hive" here with the name of the user the queries should run as
+    Connection con = DriverManager.getConnection("jdbc:hive2://l-kaxy01.hadoop.com:10000/default", "root", "123456");
+    Statement stmt = con.createStatement();
+    String tableName = "testHiveDriverTable";
+ 
+    // select * query
+    String sql = "select * from " + tableName;
+    System.out.println("Running: " + sql);
+    ResultSet res = stmt.executeQuery(sql);
+    while (res.next()) {
+      System.out.println(String.valueOf(res.getInt(1)) + "\t" + res.getString(2));
+    }
+  }
+}
