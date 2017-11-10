e6c5c2d51e35119e6011788555255ad8911dd177
DESKTOP-2DC3SS2\L-kaxy <770619071@qq.com>
2017-11-10 15:37:14 +0800

    no message

---
+++ RemoveQuotesUDF.java
@@ -0,0 +1,14 @@
+package com.l_kaxy.hadoop.hadoop_udf;
+ 
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+ 
+public final class RemoveQuotesUDF extends UDF {
+	public Text evaluate(final Text s) {
+		Text result = null
+		if (s != null) {
+			result = new Text(s.toString().replaceAll("\"", ""));
+		}
+		return result;
+	}
+}
