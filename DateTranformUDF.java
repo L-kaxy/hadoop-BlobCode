fd2cd4b08db3c81b38f3faf97d6f095349d932d5
DESKTOP-2DC3SS2\L-kaxy <770619071@qq.com>
2017-11-10 16:37:56 +0800

    no message

---
+++ DateTranformUDF.java
@@ -0,0 +1,28 @@
+package com.l_kaxy.hadoop.hadoop_udf;
+ 
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+ 
+public final class RemoveQuotesUDF extends UDF {
+
+	private final SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", Locale.ENGLISH);
+	private final SimpleDateFormat outputFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+
+	public Text evaluate(final Text s) {
+		Text result = null;
+		if (s != null) {
+
+			String inputDate = s.toString().trim();
+
+			try {
+
+				Date date = inputFormat.parse(inputDate);
+				result = new Text(outputFormat.format(date));
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+}
