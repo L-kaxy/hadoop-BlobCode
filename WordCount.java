0b8db7d91508a6ba86d7cf165310a883b06b00f3
DESKTOP-2DC3SS2\L-kaxy <770619071@qq.com>
2017-11-14 20:39:24 +0800

    2

--- WordCount.java
+++ WordCount.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;1
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
